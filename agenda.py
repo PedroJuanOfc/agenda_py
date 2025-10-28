f973faa1af28cf725237037630638b82ee13cfdf
Pedro Juan <pedrosaraivaofc@gmail.com>
2025-10-27 22:25:23 -0300

    Função de adicionar o contato

--- agenda.py
+++ agenda.py
@@ -1,3 +1,10 @@
+def adicionar_contato(contatos, nome_contato, telefone_contato, favorito=False):
+  contato = {"Nome:": nome_contato, "Telefone:": telefone_contato, "Favorito: ": favorito}
+  contatos.append(contato)
+  return
+
+contatos = []
+
 while True:
   print("\nAgenda de contatos")
   print("1. Adicionar contato")
@@ -7,7 +14,11 @@ while True:
   print("5. Excluir contato")
   print("6. Sair")
 
-  opcao = input("O que deseja fazer?")
+  opcao = input("O que deseja fazer: ")
 
-  if opcao == "6":
+  if opcao == "1":
+    nome_contato = input("Digite o nome do contato: ")
+    telefone_contato = input("Digite o telefone do contato: ")
+    adicionar_contato(contatos, nome_contato, telefone_contato)
+  elif opcao == "6":
     break
