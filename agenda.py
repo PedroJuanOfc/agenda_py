48f9f8985da46c3a9db96a7d168189ea50da02fc
Pedro Juan <pedrosaraivaofc@gmail.com>
2025-10-27 21:45:10 -0300

    Iniciando a Agenda Python

---
+++ agenda.py
@@ -0,0 +1,13 @@
+while True:
+  print("\nAgenda de contatos")
+  print("1. Adicionar contato")
+  print("2. Ver contatos")
+  print("3. Editar contato")
+  print("4. Favoritar contato")
+  print("5. Excluir contato")
+  print("6. Sair")
+
+  opcao = input("O que deseja fazer?")
+
+  if opcao == "6":
+    break
